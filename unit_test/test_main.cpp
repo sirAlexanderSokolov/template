d4c11ceaa1d79bf8983562b3233d1b12a73ca0ab
sirAlexanderSokolov <reg@shadoffarts.ru>
2023-04-30 17:28:44 +0500

    coverage

--- unit_test/test_main.cpp
+++ unit_test/test_main.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
+#include "../lib/lib_logger/logger.hpp"
 
-int main(int argc, char *argv[]) {
+using namespace std;
+
+TEST(Employee_Constructor, AllValidArgs_ReturnSuccess) {
+  logger::instance().print("asd");
+}
+
+int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
